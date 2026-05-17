450d1a99ff69904f4df6fc20847861ad151b6310
Giacomo Stelluti Scala <gsscoder@gmail.com>
2026-05-17 15:01:15 +0200

    chore: add logo to info subcommand

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -4,6 +4,7 @@ import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import chalk from 'chalk';
+import figlet from 'figlet';
 import envPaths from 'env-paths';
 import type { Command } from 'commander';
 import { isPackageJson } from '../types.js';
@@ -20,6 +21,8 @@ function handleInfo(): void {
   const repoPath = path.join(envPaths('scopy', { suffix: '' }).data, 'repos');
   const registerPath = path.join(envPaths('scopy', { suffix: '' }).data, 'scopy-register.json');
 
+  console.log(chalk.cyan(figlet.textSync('scopy', { font: 'Small Slant' }).replace(/^\n+/, '\n')));
+  console.log();
   console.log(`${chalk.bold.white(pkg.name)} ${chalk.dim(`v${pkg.version}`)}`)
   keyValue('config', configPath);
   keyValue('clone path', repoPath);
